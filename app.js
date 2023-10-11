4bea893fc34cf11ec009c755283375731bac6897
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-10-11 22:20:53 +0700

    [add] install and init bodyparser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import airRoutes from "./routes/air.js";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("ETL Pipeline");
 });
 app.use(express.json());
+app.use(bodyParser.json());
 app.use("/api/air", airRoutes);
 
 app.listen(port, () => {
